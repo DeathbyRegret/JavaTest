96bc37a30244d1c2ad9c075fbe6c72e5965fc8f4
Travis <shankstravis90@gmail.com>
2016-07-08 22:29:42 -0400

    - Added hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // Added Hello World
+        System.out.println("Hello World");
     }
 }
